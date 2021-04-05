ce5d345c136c04d936b67ff61399269301530777
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-04-05 11:46:59 -0500

    welcome css done

--- client/src/components/HeroImage.jsx
+++ client/src/components/HeroImage.jsx
@@ -4,18 +4,18 @@ import "./HeroImage.css";
 export default function HeroImage() {
   return (
     <div className="hero">
-      <div className="welcome">
-        <div className="intro">
-          <div className="header">Welcome！</div>
-          <div className="sub-header">I'm Allison</div>
-          <div className="paragraph">
-            Traveling has been a part of my life for 15 years. Explore the site
-            to find little gems travelors have discovered on their journeys.
-          </div>
-          <div className="paragraph-2">⋯ Happy Travels ⋯</div>
-          <div className="border"></div>
+      {/* <div className="welcome"> */}
+      <div className="intro">
+        <div className="header">Welcome！</div>
+        <div className="sub-header">I'm Allison</div>
+        <div className="paragraph">
+          Traveling has been a part of my life for 15 years. Explore the site to
+          find little gems travelors have discovered on their journeys.
         </div>
+        <div className="paragraph-2">⋯ Happy Travels ⋯</div>
+        <div className="border"></div>
       </div>
+      {/* </div> */}
     </div>
   );
 }
