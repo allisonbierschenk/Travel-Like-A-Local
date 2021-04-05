906d45c1ae89d8f8d39e877cb47a4e0b9f2f644e
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-04-05 12:50:45 -0500

    not now button on modal remove x button on modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -7,11 +7,6 @@ export default function Modal(props) {
   return (
     <div className="modal-container">
       <div className="modal" onClick={(e) => e.stopPropagation()}>
-        <Link>
-          <p className="x" onClick={(e) => handleOpen(false)}>
-            X
-          </p>
-        </Link>
         <p>Please signin or signup to leave a comment</p>
         <div className="modal-buttons">
           <button className="modal-button">
@@ -23,6 +18,12 @@ export default function Modal(props) {
             <Link className="button-text" to="/signup">
               REGISTER
             </Link>
+          </button>
+          <button
+            onClick={(e) => handleOpen(false)}
+            className="modal-button-notnow"
+          >
+            NOT NOW
           </button>
         </div>
       </div>
