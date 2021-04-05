43a7c4e61771fc9c983de31d78a28daf3977bfe0
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-04-05 13:54:21 -0500

    post preview css sizing smaller

--- client/src/components/PostPreview.jsx
+++ client/src/components/PostPreview.jsx
@@ -7,9 +7,9 @@ export default function PostPreview(props) {
   const { title, destination, username } = post;
 
   return (
-    <div>
+    <div className="post-preview">
       <Link to={`/posts/${post.id}`}>
-        <div className="post-preview">
+        <div>
           <h3 className="title">{title}</h3>
           <h3 className="destination">{destination}</h3>
           <h3 className="username">{username}</h3>
