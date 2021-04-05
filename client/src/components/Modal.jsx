cac70ca282f4bada9b81272c9c15430f591e17d4
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-04-05 12:39:31 -0500

    exit modal css

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -5,8 +5,13 @@ import "./Modal.css";
 export default function Modal(props) {
   const { open, handleOpen } = props;
   return (
-    <div className="modal-container" onClick={(e) => handleOpen(false)}>
+    <div className="modal-container">
       <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <Link>
+          <p className="x" onClick={(e) => handleOpen(false)}>
+            X
+          </p>
+        </Link>
         <p>Please signin or signup to leave a comment</p>
         <div className="modal-buttons">
           <button className="modal-button">
