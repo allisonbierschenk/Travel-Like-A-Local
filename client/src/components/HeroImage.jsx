687dc6eea170e8fab8ddb5994095ca4465de0c61
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-04-05 10:45:58 -0500

    hero updates

--- client/src/components/HeroImage.jsx
+++ client/src/components/HeroImage.jsx
@@ -2,5 +2,20 @@ import React from "react";
 import "./HeroImage.css";
 
 export default function HeroImage() {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <div className="welcome">
+        <div className="intro">
+          <div className="header">Welcome！</div>
+          <div className="sub-header">I'm Allison</div>
+          <div className="paragraph">
+            Traveling has been a part of my life for 15 years. Explore the site
+            to find little gems travelors have discovered on their journeys.
+          </div>
+          <div className="paragraph-2">⋯ Happy Travels ⋯</div>
+          <div className="border"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
